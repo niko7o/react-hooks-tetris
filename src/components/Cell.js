28ec1b57346dc9413cf376f98fa14ebe8115b742
Nikoto <nikotomad@gmail.com>
2019-08-15 23:43:36 +0200

    Improved Cell logic + styles

--- src/components/Cell.js
+++ src/components/Cell.js
@@ -1,7 +1,11 @@
 import React from 'react';
 
+import { StyledCell } from './styles/StyledCell';
+
+import { TETROMINOS } from '../utils/tetrominos';
+
 const Cell = ({ type }) => (
-  <div>cell</div>
+  <StyledCell type={'L'} color={TETROMINOS['L'].color} />
 );
 
 export default Cell;
