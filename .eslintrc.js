de0933158ed6160ab23d51d382a3b713a35c5f90
Nikoto <nikotomad@gmail.com>
2019-08-15 22:44:44 +0200

    added new rule to ignore array constructor warnings

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,7 +21,8 @@ module.exports = {
     'react',
   ],
   rules: {
-    'react/jsx-filename-extension': [1, { "extensions": [".js", ".jsx"] }]
+    'react/jsx-filename-extension': [1, { "extensions": [".js", ".jsx"] }],
+    'no-array-constructor': 0
   },
 };
 
