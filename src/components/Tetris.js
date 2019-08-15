9075cac28c9718b518fd585eb0a740dadca52ec5
Nikoto <nikotomad@gmail.com>
2019-08-16 00:05:00 +0200

    updated Tetris with styles

--- src/components/Tetris.js
+++ src/components/Tetris.js
@@ -4,20 +4,24 @@ import Stage from './Stage';
 import Display from './Display';
 import Button from './Button';
 
+import { StyledTetris, StyledTetrisWrapper } from './styles/StyledTetris';
+
 import { createStage } from '../utils/game-helpers';
 
 const Tetris = () => (
-  <>
-    <Stage stage={createStage()} />
-    <aside>
-      <>
-        <Display text="Score" />
-        <Display text="Rows" />
-        <Display text="Level" />
-      </>
-      <Button text="Start" />
-    </aside>
-  </>
+  <StyledTetrisWrapper>
+    <StyledTetris>
+      <Stage stage={createStage()} />
+      <aside>
+        <div>
+          <Display text="Score" />
+          <Display text="Rows" />
+          <Display text="Level" />
+        </div>
+        <Button text="Start" />
+      </aside>
+    </StyledTetris>
+  </StyledTetrisWrapper>
 );
 
 export default Tetris;
