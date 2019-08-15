2c9df292d2d20a242c6e5c7c6d3bc1afb9e02a1d
Nikoto <nikotomad@gmail.com>
2019-08-16 01:25:21 +0200

    style fix

--- src/components/styles/StyledTetris.js
+++ src/components/styles/StyledTetris.js
@@ -13,6 +13,7 @@ export const StyledTetrisWrapper = styled.div`
 export const StyledTetris = styled.div`
     display: flex;
     align-items: flex-start;
+    justify-content: center;
     padding: 40px;
     margin: 0 auto;
     max-width: 900px;
