05ad72f4f3b07249abb0f329e81869665c951f94
Nikoto <nikotomad@gmail.com>
2019-08-15 23:27:50 +0200

    new randomTetromino func

--- src/utils/game-helpers.js
+++ src/utils/game-helpers.js
@@ -8,4 +8,7 @@ export const STAGE_HEIGHT = 20;
 export const createStage = () =>
     Array.from(Array(STAGE_HEIGHT), () =>
         new Array(STAGE_WIDTH).fill([0, 'clear'])
-    )
+    );
+
+export const randomTetromino = () =>
+    Math.floor(Math.random() * 'JOLSITZ'.length);
