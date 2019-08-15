b2ce2621d0a5f61bb7292da651370b4b1f87c6cd
Nikoto <nikotomad@gmail.com>
2019-08-16 00:09:48 +0200

    changed some order + added log

--- src/components/Tetris.js
+++ src/components/Tetris.js
@@ -8,20 +8,24 @@ import { StyledTetris, StyledTetrisWrapper } from './styles/StyledTetris';
 
 import { createStage } from '../utils/game-helpers';
 
-const Tetris = () => (
-  <StyledTetrisWrapper>
-    <StyledTetris>
-      <Stage stage={createStage()} />
-      <aside>
-        <div>
-          <Display text="Score" />
-          <Display text="Rows" />
-          <Display text="Level" />
-        </div>
-        <Button text="Start" />
-      </aside>
-    </StyledTetris>
-  </StyledTetrisWrapper>
-);
+const Tetris = () => {
+  console.log(createStage);
+
+  return (
+    <StyledTetrisWrapper>
+      <StyledTetris>
+        <Stage stage={createStage()} />
+        <aside>
+          <div>
+            <Display text="Level" />
+            <Display text="Score" />
+            <Display text="Lines broken" />
+          </div>
+          <Button text="Let's start" />
+        </aside>
+      </StyledTetris>
+    </StyledTetrisWrapper>
+  );
+}
 
 export default Tetris;
